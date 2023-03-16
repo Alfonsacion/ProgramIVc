7f841358bb426d8d6f9d2994ee68bffee27e0878
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-16 18:22:59 +0100

    funcion agregar usuario definida

--- db/connect/connect.h
+++ db/connect/connect.h
@@ -1,6 +1,8 @@
 #define _CONNECT_H_
 #ifndef _CONNECT_H_
+#include "struct.h"
 
     int gestionaError (sqlite3 *db);
-    
+    void agregarUsuario(Usuario *lista, const Usuario *usuario);
+
 #endif
