cc7976d9d1e1df5df9a05c94c644eeb227cf31c9
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-16 18:22:41 +0100

    funcion agregar usuario a la base

--- db/connect/connect.c
+++ db/connect/connect.c
@@ -1,9 +1,17 @@
 #include "sqlite3.h"
 #include "connect.h"
 #include <stdio.h>
+#include "struct.h"
 
 int gestionaError (sqlite3 *db){
     fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
     return sqlite3_errcode(db);
 }
+
+//void agregarUsuario(Usuario *lista, const Usuario *usuario){
+ //   Usuario *x = lista;
+//    while (x->siguiente != NULL){
+//        x = x->siguiente;
+//    }
+//}
 
