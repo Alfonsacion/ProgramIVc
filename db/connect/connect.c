08ec36d89c39486714ef151b6215b53bcc5e4f91
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-16 17:03:20 +0100

    Librerias

--- db/connect/connect.c
+++ db/connect/connect.c
@@ -1,5 +1,9 @@
 #include "sqlite3.h"
 #include "connect.h"
+#include <stdio.h>
 
-
+int gestionaError (sqlite3 *db){
+    fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
+    return sqlite3_errcode(db);
+}
 
---
+++ db/connect/connect.h
@@ -0,0 +1,6 @@
+#define _CONNECT_H_
+#ifndef _CONNECT_H_
+
+    int gestionaError (sqlite3 *db);
+    
+#endif
