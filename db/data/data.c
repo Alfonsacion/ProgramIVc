c8fcc1a9fbc7d3192441dc6a33c862e379909cf8
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-18 18:02:53 +0100

    fallo de errormsg arreglado(creo)

--- db/data/data.c
+++ db/data/data.c
@@ -16,11 +16,11 @@ void main (void) {
     //creacion de tablas para la base de datos: acordar cuales todavia
     char *sql = "CREATE TABLE pelicula (id_pel INT PRIMARY KEY NOT NULL, nom_pel TEXT NOT NULL, duracion INT NOT NULL)";
 
-    baseDatos = sqlite3_exec(db, sql, NULL, 0, &errmsg);
+    baseDatos = sqlite3_exec(db, sql, NULL, 0, sqlite3_errmsg(db)); //errmsg es como estaba con fallo
 
     if (baseDatos != SQLITE_OK) {
-        fprintf(stderr, "Error en la creacion de la tabla: %s\n", errmsg);
-        sqlite3_free(errmsg);
+        fprintf(stderr, "Error en la creacion de la tabla: %s\n", sqlite3_errmsg(db)); //errmsg
+        sqlite3_free(sqlite3_errmsg(db)); //errmsg
     } else {
         fprintf(stdout, "La tabla se ha creado correctamente\n");
     }
