56f1297efbd6818dc6f8b1d98dac87ce5f812c3e
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-18 18:02:20 +0100

    Añadidos parametros a una funcion

--- db/connect/connect.h
+++ db/connect/connect.h
@@ -5,6 +5,6 @@
     int gestionaError (sqlite3 *db);
     void agregarUsuario(char *username, char *password);
     Usuario *nuevoUsuario(Usuario &usuario);
-    Usuario leeUsuario();
+    Usuario leeUsuario(char *usuario);
     void iniciarSesion(char *usuario, char *password);
 #endif
