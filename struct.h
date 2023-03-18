62f8ba77b4f34a568a43c83ce7ac9c6a5c654055
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-18 13:27:30 +0100

    Estructura modificada

--- struct.h
+++ struct.h
@@ -1,11 +1,12 @@
 #ifndef _STRUCT_H_
 #define _STRUCT_H_
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
 
 
-typedef struct Usuario{
-    char nombre[10];
-    int contraseyna;
-    struct Usuario* siguiente;
+typedef struct{
+    char nombreUsuario[MAX_USERNAME_LENGTH];
+    char contraseyna[MAX_PASSWORD_LENGTH];
 }Usuario;
 
 
