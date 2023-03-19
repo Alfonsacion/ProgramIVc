a6b338189990a07b166b437b96d0757d8ca2c283
IkerGlz <iker.gonrek@opendeusto.es>
2023-03-19 13:43:22 +0100

    Agragado contenido de data.c a main

--- src/main.c
+++ src/main.c
@@ -8,6 +8,38 @@
 
 void main(void)
 {
+	FILE *f = fopen("DatosUsuarios.txt", "r");
+	 sqlite3 *db;
+    int baseDatos = sqlite3_open("baseDeDatosCine.sqlite", &baseDatos);
+
+    if (baseDatos) {
+        fprintf(stderr, "No se pusdo abrir la base de datos: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    } else {
+        fprintf(stdout, "La base de datos se ha abierto correctamente\n");
+    }
+
+    //creacion de tablas para la base de datos: acordar cuales todavia
+    char *sql = "CREATE TABLE pelicula (id_pel INT PRIMARY KEY NOT NULL, nom_pel TEXT NOT NULL, duracion INT NOT NULL)";
+
+    baseDatos = sqlite3_exec(db, sql, NULL, 0, sqlite3_errmsg(db)); //errmsg es como estaba con fallo
+
+    if (baseDatos != SQLITE_OK) {
+        fprintf(stderr, "Error en la creacion de la tabla: %s\n", sqlite3_errmsg(db)); //errmsg
+        sqlite3_free(sqlite3_errmsg(db)); //errmsg
+    } else {
+        fprintf(stdout, "La tabla se ha creado correctamente\n");
+    }
+
+    char *sql = "CREATE TABLE usuario (nombreUsuario TEXT PRIMARY KEY, password TEXT);";
+    baseDatos = sqlite3_exec(db, sql, NULL, 0, NULL);
+    if (baseDatos != SQLITE_OK) {
+    fprintf(stderr, "Error al crear la tabla: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return 1;
+    }
+	
 	char nombreUsuario[MAX_USERNAME_LENGTH];
     char contraseyna[MAX_PASSWORD_LENGTH];
 
@@ -51,13 +83,10 @@ void main(void)
 			 }
 		}
 
-	sqlite3 *db;
-	const char *filename = "baseDeDatosCine.sqlite"; 
 	Usuario *lista = NULL;
-	FILE *f = fopen("DatosUsuarios.txt", "r");
 	Usuario usuario;
 
-	if (sqlite3_open(filename, &db) != SQLITE_OK){
+	if (sqlite3_open(baseDatos, &db) != SQLITE_OK){
 		return gestionaError(db);
 	}
 
