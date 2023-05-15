f7959b57a4e7b6274282212b99fd1f5978e221a7
Tu nombre <igoalonso@gmail.com>
2023-05-15 21:02:28 +0200

    tareasAdmin

---
+++ src/administrador/administrador.c
@@ -0,0 +1,9 @@
+//Menu parecido al cliente
+//Por ejemplo añadir/quitar usuarios, o añadir/quitar peliculas
+
+//Funciones inventadas, aunque no tengan nada que ver con el programa principal para ir aplicando lo dado en clase de c++
+
+
+
+
+//Clase supererior a cliente y administrador, de 'Usuario', con variables comunes (nombre, apellido......)
