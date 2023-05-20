5731aca622a0d7f987c84d38cac7e20be7d0314f
Markos Agote <markosagote@opendeusto.es>
2023-05-20 13:24:18 +0200

    durisimo SIN ERRORES

--- src/administrador/administrador.cpp
+++ src/administrador/administrador.cpp
@@ -1,17 +1,14 @@
 #include "administrador.h"
 #include <iostream>
-
-
 using namespace std;
 
-
-Administrador::Administrador(const char * nombre, unsigned int sueldo): Persona(nombre)
+Administrador::Administrador(const char * nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo): Persona(nombre, mail, tlfn, dni)
 {
     this->sueldo = sueldo;
 }
 
 
-Administrador::Administrador(const Administrador &a): Persona(a.getNombre())
+Administrador::Administrador(const Administrador &a): Persona(Persona::getNombre(), a.getMail(), a.getTlfn(), a.getDni())
 {
     this->sueldo = a.sueldo;
 }
@@ -25,7 +22,17 @@ Administrador::~Administrador()
 void Administrador::consultarDatos()
 {
     Persona::consultarDatos();
+    cout<< "Mail: " << getMail() << endl;
+    cout<< "DNI: " << getDni() << endl;
+    cout<< "Telefono: " << getTlfn() << endl;
     cout << "Sueldo: " << this->sueldo << endl;
+}
+
+
+void Administrador::anyadirAdministrador(const char* nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo)
+{
+    Administrador admin(nombre, mail, tlfn, dni, sueldo);
+    cout<<"Administrador anyadido: "<< admin.getNombre() << endl;
 }
 
 
--- src/administrador/administrador.h
+++ src/administrador/administrador.h
@@ -1,31 +1,22 @@
 #ifndef ADMINISTRADOR_H_
 #define ADMINISTRADOR_H_
-
-
 #include "persona.h"
-
 
 class Administrador: public Persona
 {
     private:
         unsigned int sueldo;
 
-
-
     public:
         Administrador();
-        Administrador(const char * nombre, unsigned int sueldo);
+        Administrador(const char * nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo);
         Administrador(const Administrador &a);
         virtual ~Administrador();
 
 
         virtual void consultarDatos();
+        void anyadirAdministrador(const char* nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo);
         void modificarDatosUsuario();//adasfs
-
-
-
-
 };
-
 
 #endif
