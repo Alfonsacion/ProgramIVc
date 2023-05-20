c9bd0b355f897e9055e80b1436cc12bbf381c0c1
Markos Agote <markosagote@opendeusto.es>
2023-05-20 14:40:59 +0200

    anyadiradmin modif coment

--- src/administrador/administrador.cpp
+++ src/administrador/administrador.cpp
@@ -1,5 +1,7 @@
 #include "administrador.h"
 #include <iostream>
+#include <string>
+#include <string.h>
 using namespace std;
 
 Administrador::Administrador(const char * nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo): Persona(nombre, mail, tlfn, dni)
@@ -31,8 +33,41 @@ void Administrador::consultarDatos()
 
 void Administrador::anyadirAdministrador(const char* nombre, const char * mail, const char * tlfn, const char * dni, unsigned int sueldo)
 {
-    Administrador admin(nombre, mail, tlfn, dni, sueldo);
-    cout<<"Administrador anyadido: "<< admin.getNombre() << endl;
+    cout<<"Has seleccionado la opción de anyadir un nuevo Administrador."<<endl;
+    cout<<"A continuacion indica sus datos: "<<endl;
+
+    char nombre[20];
+    char mail[20];
+    char tlfn[9];
+    char dni[9];
+    unsigned int sueldo;
+
+    cout << "Introduce el nombre del administrador: "<<endl;
+    //cin.getline(nombre, 20, '\n');
+
+    cout << "Introduce el mail del administrador: "<<endl;
+    //cin.getline(mail, 20, '\n');
+
+    cout<< "Introduce el telefono del administrador: "<<endl;
+    //cin.getline(tlfn, 9, '\n');
+
+    cout<< "Introduce el dni del administrador: "<<endl;
+    //cin.getline(dni,9, '\n');
+
+    cout<< "Introduce el sueldo del administrador: "<<endl;
+    cin>>sueldo;
+
+    cin.ignore();
+
+    Administrador admin (nombre, mail, tlfn, dni, sueldo);
+
+    cout<< "Administrador anyadido correctamente. "<< endl;
+
+
 }
 
 
+/*  
+    
+    Administrador admin(nombre, mail, tlfn, dni, sueldo);
+    cout<<"Administrador anyadido: "<< admin.getNombre() << endl;*/
