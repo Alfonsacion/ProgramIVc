1784a8ca1353c97d95c63ecf0164fc62a0b9d14f
Markos Agote <markosagote@opendeusto.es>
2023-05-23 08:43:52 +0200

    cliente2 call cliente process

---
+++ src/cliente2/cliente2.cpp
@@ -0,0 +1,23 @@
+#include "cliente2.h"
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+Cliente2::Cliente2(const char * nombre, const char *mail, const char * tlfn, const char * dni):Persona(nombre, mail, tlfn, dni){
+
+}
+
+Cliente2::Cliente2(const Cliente2 &c2):Persona(Persona::getNombre(), c2.getDni(), c2.getMail(), c2.getTlfn()){
+
+}
+
+Cliente2::~Cliente2(){
+
+}
+
+void Cliente2::usuarioDatos(){
+    Persona::usuarioDatos();
+    //getters y setters
+}
+
